7bbae3312ec9450f24090dbe7d481ac96b5ff5b0
Flaviane-Brum <flavianebs3@gmail.com>
2022-05-09 15:32:21 -0300

    :sparkles: Create Data

---
+++ src/data/Items.js
@@ -0,0 +1,14 @@
+import comendo from "../assets/comendo.webp";
+import correndo from "../assets/correndo.webp";
+import confuso from "../assets/confuso.webp";
+import meditando from "../assets/meditando.webp";
+import rezando from "../assets/rezando.webp";
+import snoopyOk from "../assets/snoopy-ok.webp";
+export const Items = [
+  { name: "comendo", icon: comendo },
+  { name: "correndo", icon: correndo },
+  { name: "confuso", icon: confuso },
+  { name: "meditando", icon: meditando },
+  { name: "rezando", icon: rezando },
+  { name: "snoopyOk", icon: snoopyOk },
+];
